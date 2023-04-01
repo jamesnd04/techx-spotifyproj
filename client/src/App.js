d7b8664df6d51aab9ce6a40c1be6e81f1150643d
jamesnd04 <jamesdankwah12@gmail.com>
2023-04-01 14:59:13 -0400

    feat: added new Button for getting a new playlist

--- client/src/App.js
+++ client/src/App.js
@@ -20,14 +20,20 @@ function App() {
     <div class="">
       <h1 class="h1">Welcome to DJ Spotify!</h1>
       <p1 class ="p1">This API will recommend new songs to users based on what they've previously listened to.</p1>
-      <div class="vertical-center">
+      <div class="vertical-center-logo">
         <img src ="https://logosmarcas.net/wp-content/uploads/2020/09/Spotify-Emblema.png" alt="Spotify Logo" style={{width: '400px'}}></img>
       </div>  
-      <div class="vertical-center1"><button
+      <div class="loginButton"><button
         onClick={getData}
         className="bg-green-500 hover:bg-blue-700 text-white font-bold py-6 px-8 rounded display:flex margin:auto"
         >
         Click to Login
+      </button></div>
+      <div class="newPlaylistB"><button
+        onClick={getData}
+        className="bg-green-500 hover:bg-blue-700 text-white font-bold py-6 px-8 rounded display:flex margin:auto"
+        >
+        Get Playlist   
       </button></div>
       <div class="vertical-center3">
         <p className="text-white">{data ? data["myattribute"] : "No data yet"}</p>
