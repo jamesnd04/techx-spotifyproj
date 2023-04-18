6741e5b5d75596f46827be2442d4b32692c596bd
jamesnd04 <jamesdankwah12@gmail.com>
2023-04-18 12:34:45 -0400

    <docs> added react-router-dom and removed audio visualiser for now

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Playlist from "./Playlist";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+   <Router>
+    <Routes>
+    <Route exact path = '/' Component={App} />
+    <Route exact path = '/Playlist' Component={Playlist} />
+    </Routes>
+   </Router> 
   </React.StrictMode>
 );
